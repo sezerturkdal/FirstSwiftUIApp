9b61b780c078e1c4a8646456565d106687c3de45
Sezer Turkdal <sezer.turkdal@hotmail.com>
2023-01-03 21:53:53 +0300

    Initial Commit

---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Sezer on 3.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
